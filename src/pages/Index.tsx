d6107c8922f5c445f58fe3af7b4c397b59d5138d
Juan <juanmetchart@gmail.com>
2026-01-19 18:19:48 -0300

    Add instagram account

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -103,6 +103,19 @@ export default function Index() {
             <Users className="h-6 w-6" />
             JUGAR
           </Button>
+            <div className="pt-2 flex justify-center">
+              <a
+                href="https://www.instagram.com/impostorargentino"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-flex items-center gap-2 text-pink-500 hover:underline font-medium text-sm"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-5 w-5">
+                  <path d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5A4.25 4.25 0 0 0 20.5 16.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5zm4.25 3.25a5.25 5.25 0 1 1 0 10.5a5.25 5.25 0 0 1 0-10.5zm0 1.5a3.75 3.75 0 1 0 0 7.5a3.75 3.75 0 0 0 0-7.5zm5.13.62a1.13 1.13 0 1 1-2.25 0a1.13 1.13 0 0 1 2.25 0z" />
+                </svg>
+                Instagram @impostorargentino
+              </a>
+            </div>
         </div>
 
         {/* How to Play */}
